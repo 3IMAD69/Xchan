d7903ae10721a50a966b0a28b3c119bdc023f11d
3IMAD69 <dhaimy.imad@gmail.com>
2025-05-13 19:45:12 +0100

    added Image Provider for OP

--- components/thread-detail.tsx
+++ components/thread-detail.tsx
@@ -4,7 +4,8 @@ import { Thread } from "4chan-ts";
 import Reply from "@/components/reply";
 import { Bookmark, MessageCircle, Share } from "lucide-react";
 import Image from "next/image";
-import { PhotoProvider } from "react-photo-view";
+import { useState } from "react";
+import { PhotoProvider, PhotoView } from "react-photo-view";
 import { formatContent } from "./formatContent";
 
 interface ThreadDetailProps {
@@ -18,6 +19,8 @@ export default function ThreadDetail({
   replies,
   boardId,
 }: ThreadDetailProps) {
+  const [imageURL, setImageURL] = useState(``);
+
   // const [showShareOptions, setShowShareOptions] = useState(false)
   // const { toast } = useToast()
 
@@ -59,15 +62,60 @@ export default function ThreadDetail({
             </div>
 
             {op.tim && op.ext && (
-              <div className="mt-2 rounded-md overflow-hidden">
-                <Image
-                  src={`https://i.4cdn.org/${boardId}/${op.tim}${op.ext}`}
-                  alt="op image"
-                  width={600}
-                  height={400}
-                  className="w-full h-auto max-h-[500px] object-contain"
-                />
-              </div>
+              <PhotoProvider
+                maskOpacity={0.7}
+                toolbarRender={({ onRotate, rotate, onScale, scale }) => {
+                  return (
+                    <>
+                      <svg
+                        className="PhotoView-Slider__toolbarIcon"
+                        onClick={() => onRotate(rotate + 90)}
+                        width="44"
+                        height="44"
+                        fill="white"
+                        viewBox="0 0 768 768"
+                      >
+                        <path d="M565.5 202.5l75-75v225h-225l103.5-103.5c-34.5-34.5-82.5-57-135-57-106.5 0-192 85.5-192 192s85.5 192 192 192c84 0 156-52.5 181.5-127.5h66c-28.5 111-127.5 192-247.5 192-141 0-255-115.5-255-256.5s114-256.5 255-256.5c70.5 0 135 28.5 181.5 75z" />
+                      </svg>
+                      <svg
+                        className="PhotoView-Slider__toolbarIcon"
+                        width="44"
+                        height="44"
+                        viewBox="0 0 768 768"
+                        fill="white"
+                        onClick={() => onScale(scale + 0.5)}
+                      >
+                        <path d="M384 640.5q105 0 180.75-75.75t75.75-180.75-75.75-180.75-180.75-75.75-180.75 75.75-75.75 180.75 75.75 180.75 180.75 75.75zM384 64.5q132 0 225.75 93.75t93.75 225.75-93.75 225.75-225.75 93.75-225.75-93.75-93.75-225.75 93.75-225.75 225.75-93.75zM415.5 223.5v129h129v63h-129v129h-63v-129h-129v-63h129v-129h63z" />
+                      </svg>
+                      <svg
+                        className="PhotoView-Slider__toolbarIcon"
+                        width="44"
+                        height="44"
+                        viewBox="0 0 768 768"
+                        fill="white"
+                        onClick={() => onScale(scale - 0.5)}
+                      >
+                        <path d="M384 640.5q105 0 180.75-75.75t75.75-180.75-75.75-180.75-180.75-75.75-180.75 75.75-75.75 180.75 75.75 180.75 180.75 75.75zM384 64.5q132 0 225.75 93.75t93.75 225.75-93.75 225.75-225.75 93.75-225.75-93.75-93.75-225.75 93.75-225.75 225.75-93.75zM223.5 352.5h321v63h-321v-63z" />
+                      </svg>
+                    </>
+                  );
+                }}
+              >
+                <PhotoView src={imageURL}>
+                  <div className="mt-2 rounded-md overflow-hidden cursor-pointer">
+                    <Image
+                      src={`https://i.4cdn.org/${boardId}/${op.tim}${op.ext}`}
+                      alt="op image"
+                      width={600}
+                      height={400}
+                      className="w-full h-auto max-h-[500px] object-contain"
+                      onLoad={(e) => {
+                        setImageURL(e.currentTarget.src);
+                      }}
+                    />
+                  </div>
+                </PhotoView>
+              </PhotoProvider>
             )}
 
             <div className="mt-3 flex items-center justify-between text-gray-500">
